e0cca473d50b150db21b8ae3a8798f43c29eb8b1
monarch-beluga <2744296237@qq.com>
2021-03-28 17:47:09 +0800

    2021-03-28 17:47:09

--- Python/python38/temp.py
+++ Python/python38/temp.py
@@ -1,7 +1,3 @@
-s = input()
-d = 0
-while s:
-    d = d + 2 ** (len(s)-1) * int(s[0])
-    s = s[1:]
 
-print(d)
+a = {"a": 1, "b": 2}
+
--- 自动提交/automatic1.bat
+++ 自动提交/automatic1.bat
@@ -1,4 +1,4 @@
 git add --all
-git commit -m "2021-03-27 23:46:28"
+git commit -m "2021-03-28 17:47:09"
 git pull origin master
 git push origin master
