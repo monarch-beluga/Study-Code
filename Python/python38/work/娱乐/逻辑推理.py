f6479f50fb6055c6d4c7b2fe640770b114583386
monarch-beluga <2744296237@qq.com>
2021-04-03 14:52:26 +0800

    2021-04-03 14:52:26

--- Python/python38/work/娱乐/逻辑推理.py
+++ Python/python38/work/娱乐/逻辑推理.py
@@ -6,9 +6,9 @@ def fun(x):
     if x[0] == 4:
         x[1:] = fun(x[1:])
     return x % 4
-        
-        
-answer = np.array([0]*10)
+
+
+answer = np.array([0] * 10)
 d2 = np.array([2, 3, 0, 1])
 d3 = np.array([[6, 2, 4], [2, 3, 4], [3, 4, 6], [2, 3, 6]])
 d4 = np.array([[1, 5], [2, 7], [1, 9], [6, 10]])
@@ -25,19 +25,19 @@ while len(answer[answer == 3]) != 10:
     answer = fun(answer)
     if d2[answer[1]] != answer[4]:
         continue
-    if len(set(answer[d3[answer[2]]-1])) != 1:
+    if len(set(answer[d3[answer[2]] - 1])) != 1:
         continue
-    if len(set(answer[d4[answer[3]]-1])) != 1:
+    if len(set(answer[d4[answer[3]] - 1])) != 1:
         continue
-    if answer[d5[answer[4]]-1] != answer[4]:
+    if answer[d5[answer[4]] - 1] != answer[4]:
         continue
-    if (answer[d6[answer[5]]-1][0] != answer[7]) or (answer[d6[answer[5]]-1][1] != answer[7]):
+    if (answer[d6[answer[5]] - 1][0] != answer[7]) or (answer[d6[answer[5]] - 1][1] != answer[7]):
         continue
     if np.argmin(np.bincount(answer)) != d7[answer[6]]:
         continue
-    if abs(answer[d8[answer[7]]-1]-answer[0]) == 1:
+    if abs(answer[d8[answer[7]] - 1] - answer[0]) == 1:
         continue
-    if (answer[0] == answer[5]) == (answer[d9[answer[8]]-1] == answer[4]):
+    if (answer[0] == answer[5]) == (answer[d9[answer[8]] - 1] == answer[4]):
         continue
     if max(np.bincount(answer)) - min(np.bincount(answer)) == d10[answer[9]]:
         break
--- 自动提交/automatic1.bat
+++ 自动提交/automatic1.bat
@@ -1,4 +1,4 @@
 git add --all
-git commit -m "2021-04-03 01:27:10"
+git commit -m "2021-04-03 14:52:26"
 git pull origin master
 git push origin master
