e4748f68ba2387c3c7c45a4e158a752cc7ba5bab
monarch-beluga <2744296237@qq.com>
2021-11-24 09:13:52 +0800

    2021-11-24 09:13:50

--- Python/python38/temp.py
+++ Python/python38/temp.py
@@ -8,138 +8,72 @@ author:Monarch
 @modify:
 """
 
-# 最终的ECA处理
-import work.Asia.eca as eca
-from concurrent.futures.thread import ThreadPoolExecutor
-from glob import glob
-import rasterio
+from Monarch.import_me_data import *
+import geopandas as gpd
+import pandas as pd
 import numpy as np
 import os
-from work.Asia.筛选栅格数据 import select_date
-import time
-
-paths = [r"H:/Monarch/Data/TAVG/", r'H:/Monarch/Data/NDVI/']
-outpath = "H:/Monarch/Data/ECA/05-06/"
-time_start = "1982"
-time_end = "2016"
-select_moth = ["05-01", "06-01"]
-path_judgments = ["H:/Monarch/Data/05-06/TAVG/", "H:/Monarch/Data/05-06/NDVI/"]
-out_files = [r'LST_min.tif', r'LST_max.tif']
-st = time.time()
 
 
-def Eca(dataA, dataB, dataC):
-    if ((np.isnan(dataA).sum() + np.isnan(dataB).sum()) > 0) or (len(dataA[dataA == 1]) != len(dataB[dataB == 1])) or \
-            (len(dataA[dataA == 1]) < len(dataA) // 10):
-        data1 = np.nan
-    else:
-        data1 = eca.ECA(dataA, dataB, delT=0)[0]
-    if ((np.isnan(dataA).sum() + np.isnan(dataC).sum()) > 0) or (len(dataA[dataA == 1]) != len(dataC[dataC == 1])) or \
-            (len(dataA[dataA == 1]) < len(dataA) // 10):
-        data2 = np.nan
-    else:
-        data2 = eca.ECA(dataA, dataC, delT=0)[0]
-    if np.isnan(data1) and np.isnan(data2):
-        return np.nan
-    elif np.isnan(data1):
-        return data2
-    elif np.isnan(data2):
-        return data1
-    if data1 < data2:
-        return data2
-    else:
-        return -data1
+def df_format(fp, f_mat, Data):
+    for i in range(len(Data)):
+        print(f_mat.format(*Data.loc[i].to_list()), file=fp)
 
 
+path = r'E:\public\sjy'
+select_mod = 1
+shp_roi = r'roi.shp'
+host = "103.46.128.21"
+pwd = "123456"
+port = 29611
+sql_name = "root"
+db = "meteodata"
+types = ['MTEM', 'PREP']
+start_time = '2019-06-01'
+end_time = '2019-09-01'
+sep_day = 8
 
+os.chdir(path)
 
+roi = gpd.read_file(shp_roi)
+crs = roi.crs
+wgs_roi = roi.to_crs('epsg:4326')
+# gdf = creat_station_geopandas(db, sql_name, pwd, host, port, shp_roi)
+# geo = gpd.overlay(gdf, wgs_roi, 'intersection')[['Code', 'Station', 'Lat', 'Lon', 'elev', 'geometry']]
+# geo.to_file(r'select_station.shp', encoding='utf-8')
+# print('station select success!!')
 
-def read_data(file):
-    with rasterio.open(file) as src:
-        profile = src.profile
-        temp = src.read()[0].astype("float32")
-        temp[temp == profile.data['nodata']] = np.nan
-    return temp
+geo = gpd.read_file(r'select_station.shp').to_crs(crs)
+geo['X'] = geo.geometry.x
+geo['Y'] = geo.geometry.y
+station = geo['Code'].tolist()
+# data = get_data_by_stations(station, types, start_time, end_time, db, sql_name, pwd, host, port)
+# data.to_csv(r'station_data.txt', index=False, float_format='%.2f')
+df = pd.read_csv(r'station_data.txt', header=0)
+df['date'] = df['Year']*10000+df['Month']*100+df['Day']
+start_year = int(start_time[:4])
+end_year = int(end_time[:4])+1
+t = types[0]
+data = df[['Station', 'date', t]]
+year = start_year
+data1 = data[data['date']//10000 == year]
+data1.set_index(['Station', 'date'], inplace=True)
+data1[(data1 > 1000) | (data1 < -100)] = np.nan
+data1 = data1.unstack()
+data1.columns = [i // sep_day for i in range(len(data1.columns))]
+data1 = data1.interpolate(method='linear', limit_direction='both', axis=1)
+data1 = data1.T
+data1 = data1.groupby(data1.index).mean().T
+geo1 = geo[geo['Code'].isin(data1.index)][['Code', 'X', 'Y', 'elev']].set_index('Code')
+data1 = pd.concat([data1, geo1], axis=1)
+data1.reset_index(inplace=True)
+data1.set_index(['index', 'X', 'Y', 'elev'], inplace=True)
+data1.reset_index(inplace=True)
 
-
-def process_data(judgment_src, files_src):
-    judgments_data_min = read_data(judgment_src[0])
-    judgments_data_max = read_data(judgment_src[1])
-    j = 0
-    for temp in map(read_data, files_src):
-        data_max = np.array(temp)
-        data_min = np.array(temp)
-        data_min[~np.isnan(data_min)] = 0
-        data_max[~np.isnan(data_max)] = 0
-        data_max[temp > judgments_data_max] = 1
-        data_min[temp < judgments_data_min] = 1
-        data_min = data_min.reshape(-1, 1)
-        data_max = data_max.reshape(-1, 1)
-        # temp = temp.reshape(-1, 1)
-        if j == 0:
-            data1 = data_min
-            data2 = data_max
-            # data3 = temp
-        else:
-            data1 = np.hstack([data1, data_min])
-            data2 = np.hstack([data2, data_max])
-            # data3 = np.hstack([data3, temp])
-        j += 1
-    return data1, data2
-
-
-def open_src():
-    judgment = os.listdir(path_judgments[1])
-    with rasterio.open(path_judgments[1] + judgment[0]) as src_temp:
-        profile = src_temp.profile
-        profile.data['dtype'] = 'float32'
-    if not os.path.exists(outpath):
-        os.makedirs(outpath)
-    profile.data["nodata"] = -3000
-    judgment_src_s = []
-    files_src_s = []
-    for judgment_path, path in zip(path_judgments, paths):
-        judgment_files = glob(judgment_path + '*.tif')
-        files = glob(path + '*.tif')
-        files = select_date(files, time_start, time_end, select_moth)
-        judgment_src_s.append(judgment_files)
-        files_src_s.append(files)
-    print("open-----end")
-    return profile, judgment_src_s, files_src_s
-
-
-def ECA_process(judgment_src_s, files_src_s):
-    data1_min, data1_max = process_data(judgment_src_s[0], files_src_s[0])
-    data2_min, data2_max = process_data(judgment_src_s[1], files_src_s[1])
-    with ThreadPoolExecutor(max_workers=80) as worker_temp:
-        data_min = np.array([i for i in worker_temp.map(Eca, data1_min, data2_min, data2_max)]).reshape(profile['height'], profile['width'])
-        data_max = np.array([i for i in worker_temp.map(Eca, data1_max, data2_min, data2_max)]).reshape(profile['height'], profile['width'])
-    return data_min, data_max
-    # return height, width
-
-
-profile, judgment_src_s, files_src_s = open_src()
-data_W = ECA_process(judgment_src_s, files_src_s)
-data1_min, data1_max = process_data(judgment_src_s[0], files_src_s[0])
-data2_min, data2_max = process_data(judgment_src_s[1], files_src_s[1])
-# for data_W, window in zip(map(ECA_process,
-#                                [judgment_src_s] * len(windows),
-#                                [files_src_s] * len(windows),
-#                                windows,
-#                                [i for i in range(len(windows))],
-#                                [len(windows)]*len(windows)),
-#                           windows):
-#
-#     for data_w, write_src in zip(data_W, write_src_s):
-#         # print(data_w)
-#         data_w *= 100
-#         data_w[np.isnan(data_w)] = -3000
-#         write_src.write(data_w.astype(profile.data['dtype']), 1, window=window)
-# with ThreadPoolExecutor(max_workers=30) as worker:
-#     worker.map(close_file, write_src_s)
-#     for files_src, judgment_src in zip(files_src_s, judgment_src_s):
-#         worker.map(close_file, files_src)
-#         worker.map(close_file, judgment_src)
+count = data1.shape[1]
+with open(f'{t}00000.dat', 'w') as f:
+    out_format = '{:11s}' + '{:12.2f}' * 2 + '{:10.3f}' + '{:10.2f}' * (count - 4)
+    df_format(f, out_format, data1)
 
 
 
--- 自动提交/automatic1.bat
+++ 自动提交/automatic1.bat
@@ -1,4 +1,4 @@
 git add --all
-git commit -m "2021-11-18 11:12:26"
+git commit -m "2021-11-24 09:13:50"
 git pull origin master
 git push origin master
