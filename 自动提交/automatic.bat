a30146b9b16834c39a1299de3f4ec5ef85721b70
monarch-beluga <2744296237@qq.com>
2021-11-30 11:52:09 +0800

    2021-11-30 11:52:09

--- 自动提交/automatic.bat
+++ 自动提交/automatic.bat
@@ -1,2 +1,4 @@
-python automatic.py
-automatic1.bat
+git add --all
+git commit -m "%date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,2%:%time:~3,2%:%time:~6,2%"
+git pull origin master
+git push origin master
--- 自动提交/automatic1.bat
+++
@@ -1,4 +0,0 @@
-git add --all
-git commit -m "2021-11-30 08:39:52"
-git pull origin master
-git push origin master
