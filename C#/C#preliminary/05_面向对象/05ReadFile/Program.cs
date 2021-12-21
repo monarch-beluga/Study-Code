9644789c43b08329f7c476a641f62763ae4c6c82
monarch-beluga <2744296237@qq.com>
2021-12-21 09:08:13 +0800

    2021-12-21  9:08:13

---
+++ C#/C#preliminary/05_面向对象/05ReadFile/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace _05ReadFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Program t = new Program();
+            t.test();
+            
+        }
+        public void test()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread thread1 = new Thread(new ParameterizedThreadStart(Func1));
+                thread1.Start(i);
+            }
+        }
+        private void Func1(object o)
+        {
+            
+            Console.WriteLine(o);
+            Thread.Sleep(5000);
+        }
+    }
+}
--- Python/python27/temp.py
+++ Python/python27/temp.py
@@ -5,15 +5,15 @@ from glob import glob
 import os
 import time
 
-out_path = r'H:\Monarch\paper\Data\landuse'
+out_path = r'E:\Data\paper'
 os.chdir(out_path)
 arcpy.env.workspace = out_path
 
-shp_file = glob(r'G:\arcgis1\package\jx_landuse\commondata\datasets\*.shp')
+shp_file = glob(r'data_shp\*.shp')
 
 st = time.time()
-for i, shp_f in enumerate(shp_file[:1]):
-    arcpy.FeatureToRaster_conversion(shp_f, 'TDLYDM', 'JX_%02d.tif' % (i+1), 10)
+for i in range(2, len(shp_file)):
+    arcpy.FeatureToRaster_conversion(shp_file[i].decode('GBK'), 'TDLYME', 'data_tif/JX_%02d.tif' % (i+1), 10)
     print i+1, '消耗:', time.time()-st
 print '转换成功！！！'
 
