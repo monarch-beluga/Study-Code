2d330bcf1b52f4df3ba891df565a1930525a2f52
monarch-beluga <2744296237@qq.com>
2022-04-28 22:00:10 +0800

    2022-04-28 22:00:10

--- C++ and C/PAT_Problem/temp.cc
+++ C++ and C/PAT_Problem/temp.cc
@@ -1,10 +1,15 @@
 #include<iostream>
 using namespace std;
 
-typedef struct queue
+int prime(int a)
 {
-      int *data, front;
-}Q;
+    if(a == 1) return 0;
+    if(a == 2) return 1;
+    for (int i = 2; i*i < a; ++i)
+        if (a%i==0)
+            return 0;
+    return 1;
+}
 
 int main()
 {
@@ -12,40 +17,19 @@ int main()
     #else
     freopen("input.txt", "r", stdin);
     #endif
-    int n, m, k, q, tmix, p;
-    cin >> n >> m >> k >> q;
-    Q *qs = new queue[n];
-    int *qt = new int[k], *qe = new int[k];
-    for (int i = 0; i < n; ++i)
+    int n, d, sum;
+    cin >> n;
+    while(n >= 0)
     {
-        qs[i].front = 0;
-        qs[i].data = new int[m];
-        qs[i].data[m-1] = 480;
-    }
-    for (int i = 0; i < k; ++i)
-    {
-        cin >> qe[i];
-        if (i < n*m)
-            tmix = i%n;
+        cin >> d;
+        sum = 0;
+        for(int i = n; i > 0; i /= d)
+            sum = sum*d + i%d;
+        if(prime(n) && prime(sum))
+            cout << "Yes" << endl;
         else
-        {
-            tmix = 0;
-            for (int j = 1; j < n; ++j)
-                if (qs[j].data[qs[j].front] < qs[tmix].data[qs[tmix].front])
-                    tmix = j;
-        }
-        qt[i] = qs[tmix].data[(qs[tmix].front+m-1)%m];
-        qs[tmix].data[qs[tmix].front] = qt[i] + qe[i];
-        qs[tmix].front = (qs[tmix].front+1)%m;
-    }
-    for (int i = 0; i < q; ++i)
-    {
-        cin >> p;
-        --p;
-        if (qt[p] < 1020)
-            printf("%02d:%02d\n", (qt[p]+qe[p])/60, (qt[p]+qe[p])%60);
-        else
-            cout << "Sorry" << endl;
+            cout << "No" << endl;
+        cin >> n;
     }
     return 0;
 }
