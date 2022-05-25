068d0308fbd7f897f032a5ff1d4fbac15d405cbe
monarch-beluga <2744296237@qq.com>
2022-05-25 22:00:11 +0800

    2022-05-25 22:00:11

--- C++ and C/PAT_Problem/temp.cc
+++ C++ and C/PAT_Problem/temp.cc
@@ -1,29 +1,5 @@
 #include<iostream>
 using namespace std;
-#include<algorithm>
-
-typedef struct player
-{
-    int time, p, tag;
-}Pl;
-typedef struct table
-{
-    int e_time, tag, count;
-}T;
-bool cmp(Pl a, Pl b)
-{
-    if (a.time != b.time)
-        return a.time < b.time;
-    else
-        return a.tag > b.tag;
-}
-bool cmp1(Pl a, Pl b)
-{
-    if (a.tag != b.tag)
-        return a.tag > b.tag;
-    else
-        return a.time < b.time;
-}
 
 int main()
 {
@@ -31,71 +7,11 @@ int main()
     #else
     freopen("input.txt", "r", stdin);
     #endif
-    int n, m, k, H, M, S;
-    int e_time = 3600*21;
-    cin >> n;
-    Pl *ps = new player[n];
-    for (int i = 0; i < n; ++i)
-    {
-        scanf("%d:%d:%d %d %d", &H, &M, &S, &ps[i].p, &ps[i].tag);
-        ps[i].p = min(ps[i].p, 120);
-        ps[i].time = H*3600+M*60+S;
-    }
-    cin >> k >> m;
-    T *ts = new table[k];
-    for (int i = 0; i < k; ++i)
-        ts[i].count = ts[i].tag = 0;
-    for (int i = 0; i < m; ++i)
-    {
-        cin >> H;
-        ts[H-1].tag = 1;
-    }
-    sort(ps, ps+n, cmp);
-    int min_t1, min_t2, min_t, t, t1, t2, j, s, wait;
-    for (int i = 0; i < n; ++i)
-    {
-        if (i < k)
-        {
-            t = i;
-            s = ps[i].time;
-        }
-        else 
-        {
-            min_t1 = min_t2 = e_time;
-            for (j = 0; j < k; ++j)
-            {
-                if (ts[j].tag && min_t1 > ps[i].time && min_t1 < ts[j].e_time)
-                    t1 = j, min_t1 = ts[j].e_time;
-                if (!ts[j].tag && min_t2 > ps[i].time && min_t2 < ts[j].e_time)
-                    t2 = j, min_t2 = ts[j].e_time;
-            }
-            mint = min(min_t1, min_t2);
-            // for (j = 1; j < k && !(min_time <= ps[i].time && ((ps[i].tag && ts[t].tag == ps[i].tag) || !ps[i].tag)); ++j)
-            //     {
-            //         if ((min_time > ts[j].e_time && min_time > ps[i].time) || (ts[j].e_time <= ps[i].time && ((ps[i].tag && ts[j].tag == ps[i].tag) || !ps[i].tag)))
-            //         {
-            //             t = j;
-            //             min_time = ts[j].e_time;
-            //         }
-            //     }
-            if (ts[t].tag && min_time > ps[i].time)
-            {
-                for (j = i; ps[j].time <= min_time && !ps[j].tag; ++j);
-                if(i != j && ps[j].tag)
-                    sort(ps+i, ps+j+1, cmp1);
-            }
-            s = max(min_time, ps[i].time);
-        }
-        if (s >= e_time)
-            break;
-        ts[t].e_time = s + ps[i].p*60;
-        ++ts[t].count;
-        wait = (s - ps[i].time)*1.0/60 + 0.5;
-        printf("%02d:%02d:%02d", ps[i].time/3600, (ps[i].time%3600)/60, ps[i].time%60);
-        printf(" %02d:%02d:%02d %d %d\n", s/3600, (s%3600)/60, s%60, wait, t);
-    }
-    cout << ts[0].count;
-    for (int i = 1; i < k; ++i)
-        cout << " " << ts[i].count;
+    char bits[14] = "0123456789ABC";
+    int rgb[3];
+    cin >> rgb[0] >> rgb[1] >> rgb[2];
+    cout << "#";
+    for (int i = 0; i < 3; ++i)
+        cout << bits[rgb[i]/13] << bits[rgb[i]%13];
     return 0;
 }
