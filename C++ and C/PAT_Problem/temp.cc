52d196f193b00cfb1a802296075caa5b516dbbab
monarch-beluga <2744296237@qq.com>
2022-05-31 22:00:09 +0800

    2022-05-31 22:00:09

--- C++ and C/PAT_Problem/temp.cc
+++ C++ and C/PAT_Problem/temp.cc
@@ -1,6 +1,15 @@
 #include<iostream>
 using namespace std;
-#define Max 100010
+#include<algorithm>
+
+typedef struct station
+{
+    float p, d;
+}GS;
+bool cmp(GS a, GS b)
+{
+    return a.d < b.d;
+}
 
 int main()
 {
@@ -8,23 +17,44 @@ int main()
     #else
     freopen("input.txt", "r", stdin);
     #endif
-    int w1, w2, n, address, next, ll[Max][2], p;
-    char data;
-    cin >> w1 >> w2 >> n;
+    float Cmax, D, Davg, Xmax = 0, x, cost, pmin, pre_p, c = 0;
+    int n, g = 0;
+    cin >> Cmax >> D >> Davg >> n;
+    GS *ss = new station[n];
     for (int i = 0; i < n; ++i)
+        cin >> ss[i].p >> ss[i].d;
+    sort(ss, ss+n, cmp);
+    while(g < n)
     {
-        cin >> address >> data >> next;
-        ll[address][0] = next;
-        ll[address][1] = 0;
-    }
-    for(p = w1; p != -1; p = ll[p][0])
-        ++ll[p][1];
-    for (p = w2; p != -1; p = ll[p][0])
-        if (++ll[p][1] == 2)
+        x = Cmax * Davg + Xmax;
+        if (x >= D)
         {
-            printf("%05d", p);
+            cost += (D - Xmax)/Davg*pre_p;
+            printf("%.2f", cost);
             return 0;
         }
-    cout << -1;
+        for (int i = g+1; ss[i].d <= x && i < n; ++i)
+            if(ss[i].p <= pmin)
+            {
+                pmin = ss[i].p;
+                g = i;
+            }
+        if (pmin > pre_p)
+        {
+            cost += (Cmax-c)*pre_p;
+            c = Cmax - (ss[g].d - Xmax)/Davg;
+        }
+        else
+        {
+            cost += ((ss[g].d - Xmax)/Davg)*pre_p;
+            c = 0;
+        }
+        Xmax = ss[g].d;
+        printf("%d %.2f %.2f %.2f %.2f %.2f\n", g, pre_p, pmin, cost, c, Xmax);
+        pre_p = ss[g].p;
+        ++g;
+        pmin = ss[g].p;
+    }
+    printf("The maximum travel distance = %.2f", x);
     return 0;
 }
