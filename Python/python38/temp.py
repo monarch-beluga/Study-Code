58377d72ea686e209200663925cdde7c3e3a8522
monarch-beluga <2744296237@qq.com>
2022-06-16 22:00:10 +0800

    2022-06-16 22:00:10

--- Python/python38/temp.py
+++ Python/python38/temp.py
@@ -8,6 +8,10 @@ author:monarch
 @modify:
 """
 
+import pandas as pd
 
-
-
+df = pd.read_csv(r'E:\Data\class\R语言区域分析\人口数据.csv', header=0, index_col=0)
+df1 = df.T
+df2 = df1.sort_index()
+df2.insert(2, 'time', range(2000, 2021))
+df2.to_csv(r'E:\Data\class\R语言区域分析\人口数据1.csv', header=True, index=True)
